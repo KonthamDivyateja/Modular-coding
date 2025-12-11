117f1ce447c0adc96c56ac96297cf2b17e33e8b6
KonthamDivyateja <139646440+KonthamDivyateja@users.noreply.github.com>
2025-12-11 18:07:33 +0530

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,10 @@
+const createFooter = () => {
+  return `
+    <footer style="text-align: center; padding: 10px; background-color: #f1f1f1; position: fixed; bottom: 0; width: 100%;">
+      <p>&copy; 2023 Modular Todo App | Project-Based Question</p>
+    </footer>
+  `;
+};
+
+
+export default createFooter;
